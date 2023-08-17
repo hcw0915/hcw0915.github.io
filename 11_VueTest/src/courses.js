b48dc80613fb9b422a0036221221df3dab2ba442
hcw0915 <love233031@gmail.com>
2023-08-17 09:49:50 +0800

    third

---
+++ 11_VueTest/src/courses.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: 1,
+    img: require("../src/assets/img/A.jpg"),
+    name: "Basic Course"
+  },
+  {
+    id: 2,
+    img: require("../src/assets/img/B.jpg"),
+    name: "Advanced Course"
+  },
+  {
+    id: 3,
+    img: require("../src/assets/img/C.jpg"),
+    name: "Extreme Course"
+  }
+];
